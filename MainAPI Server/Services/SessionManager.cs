42ce58ae847e91139caaed314329e433a02577a0
WooSH <imdlsrks.mc@gmail.com>
2025-06-20 22:34:31 +0900

    feat: implement SessionManager for WebSocket session management
    - Added SessionManager class to handle WebSocket sessions
    - Implemented methods for registering and unregistering sessions
    - Included a placeholder for sending messages to clients

---
+++ MainAPI Server/Services/SessionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace MainAPI_Server.Services
+{
+    public class SessionManager
+    {
+        private static readonly ConcurrentDictionary<string, WebSocket> _sessions = new();
+
+        /// <summary>
+        /// 새로운 세션 등록
+        /// </summary>
+        /// <param name="sessionId">세션 Id</param>
+        /// <param name="socket">소캣</param>
+        public static void Register(string sessionId, WebSocket socket)
+        {
+            _sessions[sessionId] = socket;
+        }
+
+        /// <summary>
+        /// 세션 제거
+        /// </summary>
+        /// <param name="sessionId">세션 Id</param>
+        public static void Unregister(string sessionId)
+        {
+            _sessions.TryRemove(sessionId, out _);
+        }
+
+        /// <summary>
+        /// 지정된 클라이언트 세션으로 메시지 전송
+        /// </summary>
+        /// <param name="sessionId">세션 Id</param>
+        /// <param name="message">전송할 메시지</param>
+        /// <returns></returns>
+        public static async Task SendToClientAsync(string sessionId, string message)
+        {
+            
+        }
+
+    }
+}
