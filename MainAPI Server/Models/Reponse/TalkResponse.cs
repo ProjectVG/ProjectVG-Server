48178ab97ac9cd1e6e93e9b04031aba6bdbf5c6b
WooSH <imdlsrks.mc@gmail.com>
2025-06-20 18:24:27 +0900

    feat: add TalkRequest and TalkResponse models for handling API requests and responses
    - 생성: TalkRequest, TalkResponse 모델 클래스 추가
    - 요청 메시지와 응답 구조 정의

---
+++ MainAPI Server/Models/Reponse/TalkResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MainAPI_Server.Models.Reponse
+{
+    public class TalkResponse
+    {
+        public string Id { get; set; }
+        public string Response { get; set; }
+    }
+
+}
---
+++ MainAPI Server/Models/Request/TalkRequest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MainAPI_Server.Models.Request
+{
+    public class TalkRequest
+    {
+        public string Id { get; set; }
+        public string Actor { get; set; }
+        public string Message { get; set; }
+        public string? Action { get; set; }
+    }
+
+}
