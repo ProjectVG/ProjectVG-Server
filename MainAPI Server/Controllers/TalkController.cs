7850f7cf2cedbea717002149cd08b995cdf06231
WooSH <imdlsrks.mc@gmail.com>
2025-06-20 19:11:16 +0900

    feat: enhance TalkController to process requests asynchronously
    - Implemented asynchronous processing of TalkRequest in the Post method using Task.Run
    - Added a private method to handle the processing logic for TalkRequest
    - Updated response handling to confirm receipt of the request

--- MainAPI Server/Controllers/TalkController.cs
+++ MainAPI Server/Controllers/TalkController.cs
@@ -12,12 +12,25 @@ namespace MainAPI_Server.Controllers
         public IActionResult Post([FromBody] TalkRequest request)
         {
 
+            // todo : 작업을 실재 비동기로 실행
+            Task.Run(() => {
+                ProcessTalkRequestAsync(request);
+            });
+
+            // 아래는 요청이 잘 도착했다는 메시지만 전송
             var response = new TalkResponse {
                 Id = request.Id,
                 Response = request.Message,
             };
 
             return Ok(response);
+        }
+
+        private async Task ProcessTalkRequestAsync(TalkRequest request)
+        {
+            {
+                Console.WriteLine("작업 실행");
+            }
         }
     }
 }
