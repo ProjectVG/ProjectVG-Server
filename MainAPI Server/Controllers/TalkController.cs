235f60460eff637e324270f4180ae83a25f572bc
WooSH <imdlsrks.mc@gmail.com>
2025-06-20 18:24:36 +0900

    feat: implement TalkController for handling talk requests
    - Add TalkController with a POST endpoint to process TalkRequest and return TalkResponse
    - Define API route for talk interactions

---
+++ MainAPI Server/Controllers/TalkController.cs
@@ -0,0 +1,23 @@
+﻿using MainAPI_Server.Models.Reponse;
+using MainAPI_Server.Models.Request;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MainAPI_Server.Controllers
+{
+    [ApiController]
+    [Route("api/talk")]
+    public class TalkController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] TalkRequest request)
+        {
+
+            var response = new TalkResponse {
+                Id = request.Id,
+                Response = request.Message,
+            };
+
+            return Ok(response);
+        }
+    }
+}
